b32e734eb3be00c2030378caad72c9757dfe3917
Ubuntu <ubuntu@ip-172-31-9-137.ec2.internal>
2022-05-21 13:05:20 -0500

    A14 - Final Submission

--- util/archive/archive_app.py
+++ util/archive/archive_app.py
@@ -7,22 +7,83 @@
 ##
 __author__ = 'Vas Vasiliadis <vas@uchicago.edu>'
 
-import json
-import os
+import json, requests
+import os,sys
 
-from flask import Flask
+import boto3
+from flask import Flask, request, session, jsonify
 
 app = Flask(__name__)
 environment = 'archive_app_config.Config'
 app.config.from_object(environment)
 
+
+#  Get configuration
+region_name = app.config['AWS_REGION_NAME']
+table_name = app.config['AWS_DYNAMODB_ANNOTATIONS_TABLE']
+queue_url = app.config['AWS_SQS_REQUESTS_QUEUE_URL']
+bucket_name = app.config['AWS_S3_RESULTS_BUCKET']
+state_machine_arn = app.config['STATE_MACHINE_ARN']
+
+# Import utility helpers
+sys.path.insert(1, os.path.realpath(os.path.pardir))
+import helpers
+
 @app.route('/', methods=['GET'])
 def home():
+  user_id = session['primary_identity']
+  print(user_id)
   return (f"This is the Archive utility: POST requests to /archive.")
 
 @app.route('/archive', methods=['POST'])
 def archive_free_user_data():
-  pass
+
+  # Get message 
+  message =  json.loads(request.data.decode())
+  print(message)
+  
+  if request.headers['X-Amz-Sns-Message-Type'] == 'SubscriptionConfirmation':
+    # Confirm SNS topic subscription confirmation
+    print('----------------confirm----------------')
+    response = requests.get(url = message['SubscribeURL'])
+    print(response)
+  else:  
+    # Get information from message
+    print('\n --------Process job request notification--------')
+    print(message)
+    user_id = message['user_id']
+    job_id = message['job_id']
+    s3_key_result_file = message['s3_key_result_file']
+    profile = helpers.get_user_profile(user_id)
+    user_type = profile[4]
+
+    # Check user type
+    if user_type == 'free_user':
+      # For free user, move S3 object to glacier
+      # Set the input
+      input = json.dumps({
+        'region_name' : region_name,
+        'bukcet_name' : bucket_name,
+        'job_id': job_id,
+        's3_key_result_file': s3_key_result_file,
+      })
+      
+      # Open a connection to the S3 service
+      sfn_client = boto3.client('stepfunctions', region_name = region_name)
+
+      # Execute step function
+      execution_response = sfn_client.start_execution(
+        stateMachineArn=state_machine_arn, 
+        name = job_id,
+        input=input
+      )
+      print(execution_response)
+
+  return jsonify({
+    "code": 200, 
+    "message": "Archive the S3 object."
+  }), 200
+
 
 # Run using dev server (remove if running via uWSGI)
 app.run('0.0.0.0', debug=True)
--- util/archive/archive_app_config.py
+++ util/archive/archive_app_config.py
@@ -8,6 +8,18 @@
 ##
 __author__ = 'Vas Vasiliadis <vas@uchicago.edu>'
 
+# Get the IAM username that was stashed at launch time
+try:
+  with open('/home/ubuntu/.launch_user', 'r') as file:
+    iam_username = file.read().replace('\n', '')
+except FileNotFoundError as e:
+  if ('LAUNCH_USER' in  os.environ):
+    iam_username = os.environ['LAUNCH_USER']
+  else:
+    # Unable to set username, so exit
+    print("Unable to find launch user name in local file or environment!")
+    raise e
+
 class Config(object):
 
   CSRF_ENABLED = True
@@ -15,6 +27,18 @@ class Config(object):
   AWS_REGION_NAME = "us-east-1"
 
   # AWS DynamoDB table
-  AWS_DYNAMODB_ANNOTATIONS_TABLE = "<CNetID>_annotations"
+  AWS_DYNAMODB_ANNOTATIONS_TABLE = f"{iam_username}_annotations"
+
+   # AWS SQS queues
+  AWS_SQS_WAIT_TIME = 20
+  AWS_SQS_MAX_MESSAGES = 10
+  AWS_SQS_REQUESTS_QUEUE_URL = f"https://sqs.us-east-1.amazonaws.com/127134666975/{iam_username}_a14_job_requests"
+  
+   # AWS S3 upload parameters
+  AWS_S3_INPUTS_BUCKET = "gas-inputs"
+  AWS_S3_RESULTS_BUCKET = "gas-results"
+
+  # AWS SNF 
+  STATE_MACHINE_ARN = f"arn:aws:states:us-east-1:127134666975:stateMachine:{iam_username}_a14_archive"
 
 ### EOF
